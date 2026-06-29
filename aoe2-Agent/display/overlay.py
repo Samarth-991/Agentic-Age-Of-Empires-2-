504ce6996adbaf935d867aade7b271c634136463
Samarth Tandon <samarthtandon91@gmail.com>
2026-06-29 22:25:31 +0530

    Add files via upload

--- aoe2-Agent/display/overlay.py
+++ aoe2-Agent/display/overlay.py
@@ -23,7 +23,7 @@ from rich.panel import Panel
 from rich.table import Table
 from rich.text import Text
 
-from ..schemas import GameStateSnapshot
+from schemas import GameStateSnapshot
 
 
 @dataclass
